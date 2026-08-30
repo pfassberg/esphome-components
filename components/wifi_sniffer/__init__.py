e85cd8dae7fca1d5d96863faf8dabf785534df39
Peter Fassberg <peter@fassberg.se>
2026-08-30 19:56:25 +0200

    Update __init__.py

--- components/wifi_sniffer/__init__.py
+++ components/wifi_sniffer/__init__.py
@@ -1,8 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import number
 from esphome.const import CONF_ID
 
+# Define the namespace matching your C++ file
 wifi_sniffer_ns = cg.esphome_ns.namespace('wifi_sniffer')
 WifiSniffer = wifi_sniffer_ns.class_('WifiSniffer', cg.Component)
 
@@ -13,3 +13,6 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
+    # Correct way: Inject native ESP-IDF component linking flags
+    cg.add_build_flag("-lesp_wifi")
