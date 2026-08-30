76b5f0ec06d8efadd08e6fa2f49c80d5f7cae04a
Peter Fassberg <peter@fassberg.se>
2026-08-30 19:55:27 +0200

    Update __init__.py

--- components/wifi_sniffer/__init__.py
+++ components/wifi_sniffer/__init__.py
@@ -13,4 +13,3 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add_library("esp_wifi", None)
